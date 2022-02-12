9ec68265aafaebfd601e1c1494c1f94ba561374b
madhavibansal <madhvi.mitrc@gmail.com>
2022-02-12 18:12:43 +0530

    my login-register with node and react project

--- learn_react/register/react-login/models/token.js
+++ learn_react/register/react-login/models/token.js
@@ -10,6 +10,6 @@ const tokenSchema = new Schema({
 	},
 	token: { type: String, required: true },
 	createdAt: { type: Date, default: Date.now, expires: 3600 },
-});
+})
 
 module.exports = mongoose.model("token", tokenSchema);
